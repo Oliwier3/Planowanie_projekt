b9ee8a5fba54615965c4db2d29bb4f3182c90731
piasek <piaskowski3456@gmail.com>
2022-12-05 14:44:28 +0100

    1 commit

--- app/src/main/java/com/example/planowanie_projekt/MainActivity.kt
+++ app/src/main/java/com/example/planowanie_projekt/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.planowanie_projekt
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.CalendarView
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<Button>(R.id.poczatek).setOnClickListener {
+        val ogur = findViewById<CalendarView>(R.id.calendarView).date
+        findViewById<TextView>(R.id.Pomidor).text = ogur
+        }
     }
 }
